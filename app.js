0d5f25a5a94d4d86d0af90e46dbfc80e695f6600
Puskar Simkhada <puskarsimkhada22@gmail.com>
2024-02-19 12:57:13 +0545

    hamburger added

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ hamburger.addEventListener('click', ( )=> {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
+mobile_menu.addEventListener('click', ( )=> {
+    hamburger.classList.remove('active');
+    mobile_menu.classList.remove('active');
+});
